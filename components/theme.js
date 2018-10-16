a24e55276c64711d2ebc3b625fe20e5818bb1881
Matt <meinzer.matthew@gmail.com>
2018-10-15 22:39:21 -0500

    Add a theme file for CSS variables

---
+++ components/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  black: '#262626',
+  grey: '#3A3A3A',
+  lightGrey: '#E1E1E1',
+  offWhite: '#F4F4F4',
+  blue: '#314CB6',
+  lightBlue: '#4392F1',
+  maxWidth: '860px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+export default theme;
